cb7fcf270d42f3d64b695faa300a73d9319894ea
kanj <knjterrell@mybluelight.com>
2016-07-18 17:31:20 -0700

    Create canbuster.h

---
+++ libMakeModel/canbuster.h
@@ -0,0 +1,14 @@
+/**
+ * Make Model
+ *
+ * Copyright (c) 2016 Kirk Terrell All rights reserved.
+ */
+
+
+
+// Use when 
+
+
+#define ecuBRAKE01  0x0	// Arbitration ID associated with braking
+#define ecuBRAKE02  0x0	// Arbitration ID associated with braking
+#define ecuSTEER01  0x0	// Arbitration ID associated with steering

0b171452cb35395fe51b1d4a9b913ff05091fcde
kanj <knjterrell@mybluelight.com>
2016-07-18 17:30:23 -0700

    Create canbuster.h

---
+++ libMazda3/canbuster.h
@@ -0,0 +1,14 @@
+/**
+ * Mazda 3
+ *
+ * Copyright (c) 2016 Kirk Terrell All rights reserved.
+ */
+
+
+
+
+
+
+#define ecuBRAKE01  0x190	// Arbitration ID associated with braking
+#define ecuBRAKE02  0x205	// Arbitration ID associated with braking
+#define ecuSTEER01  0x240	// Arbitration ID associated with steering
